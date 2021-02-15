b4a5d2fdff4dcdfe58aac4b0caf4aba77d9e86f1
taka-elle <takaelle0119@gmail.com>
2021-02-15 19:18:03 +0900

    正規表現の修正
    /\A[ぁ-んァ-ン一-龥]/を
    /\A[ぁ-んァ-ン一-龥々]+\z/に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname, format: { with: /\A[a-z0-9]+\z/i }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'Full-width characters.' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]\z/, message: 'Full-width characters.' }
     validates :last_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
     validates :first_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
     validates :birthday
