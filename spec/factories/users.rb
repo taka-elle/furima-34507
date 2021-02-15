b48db6ffa3068c08221aa134ca9abe42a9ffb3ae
taka-elle <takaelle0119@gmail.com>
2021-02-15 18:28:11 +0900

    テストコードの記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'Test'}
+    email                 {'test@gmail.com'}
+    password              {'test111'}
+    password_confirmation {password}
+    last_name             {'田中'}
+    first_name            {'太郎'}
+    last_name_f           {'タナカ'}
+    first_name_f          {'タロウ'}
+    birthday              {'1997-01-19'}
+  end
+end
