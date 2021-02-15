399fedd400ccfbb605ab007865cd49e711be7ad8
taka-elle <takaelle0119@gmail.com>
2021-02-15 14:51:28 +0900

    エラーメッセージの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
         with_options presence: true do
-          validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
-          validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-          validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-          validates :last_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-          validates :first_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+          validates :nickname, format: { with: /\A[a-z0-9]+\z/i}
+          validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
+          validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
+          validates :last_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
+          validates :first_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
           validates :birthday
         end
 end
