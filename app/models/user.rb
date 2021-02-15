e6d2c474d0493e7515aa8da7deaf7f9380258ffb
taka-elle <takaelle0119@gmail.com>
2021-02-15 18:42:21 +0900

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-        with_options presence: true do
-          validates :nickname, format: { with: /\A[a-z0-9]+\z/i}
-          validates :last_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
-          validates :first_name,format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters."}
-          validates :last_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
-          validates :first_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters."}
-          validates :birthday
-        end
-        validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+
+  with_options presence: true do
+    validates :nickname, format: { with: /\A[a-z0-9]+\z/i }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters.' }
+    validates :last_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
+    validates :first_name_f, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters.' }
+    validates :birthday
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
