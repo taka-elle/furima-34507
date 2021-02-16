a68730c91fa28c7f3e913525d0fc19cb7710c3d3
taka-elle <takaelle0119@gmail.com>
2021-02-16 21:39:25 +0900

    Jsファイルの修正

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -7,8 +7,6 @@ function count (){
     const price = articleText.value
     const tax = price*0.1
     const beforePrice = price - tax
-    console.log(tax)
-    console.log(beforePrice);
     const taxPrice = document.getElementById('add-tax-price');
     const profitPrice = document.getElementById('profit');
     taxPrice.innerHTML = tax;
