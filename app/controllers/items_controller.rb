d900231172f21c87282911beae137fad7b0b8d93
taka-elle <takaelle0119@gmail.com>
2021-02-17 14:13:19 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.all.includes(:user).order("created_at DESC")
+    @items = Item.all.includes(:user).order('created_at DESC')
   end
 
   def new
