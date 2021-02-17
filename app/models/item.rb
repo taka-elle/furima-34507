820b412d740b8f7dc8ab97c8dc82afdb5c799121
taka-elle <takaelle0119@gmail.com>
2021-02-17 15:46:10 +0900

    Active_hashのアソシエーション設定
    index.html.erbファイルのif文の表記を変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
   belongs_to :category
+  belongs_to :status
+  belongs_to :charges
+  belongs_to :area
+  belongs_to :day
   belongs_to :user
 
   with_options presence: true do
