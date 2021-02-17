85a725e07ec51ca4224889e441015ef2d5f574dd
taka-elle <takaelle0119@gmail.com>
2021-02-17 17:12:30 +0900

    jsファイル修正
    機能していなかったので

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,9 +1,8 @@
 const { formSubmitButtonClick } = require("@rails/ujs");
 
 function count (){
-  const articleText  = document.getElementById("item-price");
+  const articleText = document.getElementById("item-price");
   articleText.addEventListener("keyup", () => {
-
     const price = articleText.value
     const tax = price*0.1
     const beforePrice = price - tax
