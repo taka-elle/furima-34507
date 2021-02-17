6aa2808f0c0ec3cdcce353c5cf050654638fe94f
taka-elle <takaelle0119@gmail.com>
2021-02-17 18:02:42 +0900

    編集ページへの規制
    ログインしていないユーザーはログイン画面へ
    出品者ではないユーザーはトップページへ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create,:edit,:update]
+  before_action :move_to_index,only: [:edit]
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
@@ -33,5 +34,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :status_id, :charges_id, :area_id, :day_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
