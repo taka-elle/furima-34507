69323ebcca9e6ee0895f1b884159a1e5e6715c7c
taka-elle <takaelle0119@gmail.com>
2021-02-18 16:35:02 +0900

    カラム名の変更
    addressテーブルのareaカラムをarea_idに

--- db/migrate/20210218053806_create_addresses.rb
+++ db/migrate/20210218053806_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string     :add_num,    null: false
-      t.integer    :area,       null: false, index: true
+      t.integer    :area_id,       null: false, index: true
       t.string     :city,       null: false
       t.string     :add_line,   null: false
       t.string     :add_build
