968c4a3d3e1c3403f45915d8ba69d181ab3da3b1
taka-elle <takaelle0119@gmail.com>
2021-02-18 10:56:22 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,7 +35,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
