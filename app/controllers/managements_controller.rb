8324f46800d5a05db6ff37a6ce27113dcfd2736c
taka-elle <takaelle0119@gmail.com>
2021-02-20 13:45:07 +0900

    購入ページの制限

--- app/controllers/managements_controller.rb
+++ app/controllers/managements_controller.rb
@@ -1,6 +1,7 @@
 class ManagementsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item
+  before_action :move_to_index
 
 
   def index
@@ -26,6 +27,11 @@ class ManagementsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id
+    redirect_to root_path unless @item.management.blank?
   end
 
   def pay_item
